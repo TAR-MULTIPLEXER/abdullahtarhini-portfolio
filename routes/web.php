e57ca4b9f1d46333281d229bddf713057850f3f4
abdullah tarhini <abdullahtarhini55@gmail.com>
2026-05-21 19:09:26 +0300

    Add temporary admin creation route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,20 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/create-admin', function () {
+    $user = User::updateOrCreate(
+        ['email' => 'abdullahtarhini55@gmail.com'],
+        [
+            'name' => 'Abdullah Tarhini',
+            'password' => Hash::make('communication41730076'), // CHANGE THIS
+        ]
+    );
+    return 'Admin user created/updated successfully!';
+});
 Route::get('/project/{slug}', [HomeController::class, 'show'])->name('project.show');
