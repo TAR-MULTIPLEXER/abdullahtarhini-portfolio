5f134528604dd841eb42e72c34a96efb6ca673f0
abdullah tarhini <abdullahtarhini55@gmail.com>
2026-05-21 20:58:27 +0300

    Fix: Remove project seeding to stop crash

--- database/seeders/PortfolioSeeder.php
+++ database/seeders/PortfolioSeeder.php
@@ -4,28 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Project;
 use Illuminate\Support\Facades\Hash;
 
 class PortfolioSeeder extends Seeder
 {
     public function run(): void
     {
-        // 1. Create Admin User
+        // ONLY Create Admin User. No projects.
         User::updateOrCreate(
             ['email' => 'abdullahtarhini55@gmail.com'],
             [
                 'name' => 'Abdullah Tarhini',
                 'password' => Hash::make('communication41730076'), 
-            ]
-        );
-
-        // 2. Create Sample Project (Only use 'title' to avoid column errors)
-        // We will add other details via the Admin Panel later once you login!
-        Project::updateOrCreate(
-            ['title' => 'Sample Project'],
-            [
-                // Leave other fields empty for now to prevent crashes
             ]
         );
     }
